d5850445f8f46740b40f52627a948ed8e4c3d9c6
Anthony Suárez <neolight1010@gmail.com>
2021-07-13 16:51:38 -0500

    Whiteboard: Allow swapping brush color.

--- whiteboard/main.c
+++ whiteboard/main.c
@@ -1,20 +1,32 @@
 #include <raylib.h>
 #include <stdio.h>
 
+void swapMainColors(Color *primary, Color *secondary) {
+  Color temp = *primary;
+
+  *primary = *secondary;
+  *secondary = temp;
+}
+
 int main() {
   const int W = 800;
   const int H = 450;
 
   InitWindow(W, H, "Whiteboard");
 
+  Color primaryColor = RED;
+  Color secondaryColor = RAYWHITE;
+
   Vector2 brushCoords = {(float)W / 2, (float)H / 2};
-  Color brushColor = RED;
   float brushSize = 15;
   float brushVelocity = 1;
 
   while (!WindowShouldClose()) {
-    // Controls
-    // Move brush.
+    // Brush color control
+    if (IsKeyPressed(KEY_C))
+      swapMainColors(&primaryColor, &secondaryColor);
+
+    // Move brush
     if (IsKeyDown(KEY_UP))
       brushCoords.y -= brushVelocity;
     if (IsKeyDown(KEY_DOWN))
@@ -27,7 +39,7 @@ int main() {
     // Drawing
     BeginDrawing();
 
-    DrawCircleV(brushCoords, brushSize, brushColor);
+    DrawCircleV(brushCoords, brushSize, primaryColor);
 
     EndDrawing();
   }
